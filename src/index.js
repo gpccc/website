9c3bdc43154174276b262c19fb0482c4cdd2ef1e
Phu Vuong <pvuong@vuonglab.com>
2020-10-03 10:59:06 -0700

    Create Hello World button using Material-UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function App() {
+    return (
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
