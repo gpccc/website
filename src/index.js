5c87a61f9352ee56b276538212763bed2ed0ab47
Phu Vuong <pvuong@vuonglab.com>
2020-10-04 11:57:18 -0700

    Update English sermon list with today's service

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import SermonCard from './components/sermon-card';
 function App() {
     return (
         <div>
-        <SermonCard youtubeVideoID="uqmaE9JKGtc" topic="Ezra 3: First Comes Sacrifice" pastor="Pastor Larry Lee" date="September 27, 2020" />
+
+        <SermonCard youtubeVideoID="EWQ__H_85bE" topic="Ezra 4: Rebuilding Opposed" pastor="Pastor Larry Lee" date="October 4, 2020" />
 
         {/*
+        <SermonCard youtubeVideoID="uqmaE9JKGtc" topic="Ezra 3: First Comes Sacrifice" pastor="Pastor Larry Lee" date="September 27, 2020" />
         <SermonCard youtubeVideoID="GA94lEQkKo0" topic="Exodus 4:10-17: Talent" pastor="Pastor Steven Chin" date="September 20, 2020" />
         <SermonCard youtubeVideoID="Q5x9gZWP6tM" topic="Ezra 2: The Remnant" pastor="Pastor Jerry Lin" date="September 13, 2020" />
-        <SermonCard youtubeVideoID="7sqGqrruW6w" topic="Ezra 1: Return to Rebuild" pastor="Pastor Larry Lee" date="September 6, 2020" />
         
-
         <iframe width="560" height="315" src="AYadEf1YjVk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
         R0ikzK-juhI
         uOjAL6YPG3U
