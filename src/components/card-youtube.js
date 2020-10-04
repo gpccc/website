e07712c1dad01459a309cd4c2917d989e895381e
Phu Vuong <pvuong@vuonglab.com>
2020-10-04 15:42:00 -0700

    Enable YouTube IFrame API calls

--- src/components/card-youtube.js
+++ src/components/card-youtube.js
@@ -12,7 +12,7 @@ export default class YouTubeCard extends React.Component {
         component="iframe"
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/" + videoID}
+        src={"https://www.youtube.com/embed/" + videoID + "?enablejsapi=1"}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
       />
