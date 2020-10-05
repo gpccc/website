aa87655eae482395db63b1ce690d3e82d7ec259c
Phu Vuong <pvuong@vuonglab.com>
2020-10-04 17:43:02 -0700

    Add functions to seek and change a sermon video

--- src/components/card-youtube.js
+++ src/components/card-youtube.js
@@ -6,6 +6,31 @@ import CardMedia from '@material-ui/core/CardMedia';
 const ID = "youtubeFrame";
 
 export default class YouTubeCard extends React.Component {
+  static seekTo(hour, minute, second) {
+    const seconds = ((60 * hour) + minute) * 60 + second;
+
+    const iframe = document.getElementById(ID);
+
+    // https://stackoverflow.com/questions/7443578/youtube-iframe-api-how-do-i-control-an-iframe-player-thats-already-in-the-html
+    iframe.contentWindow.postMessage(JSON.stringify({
+      "event": "command",
+      "func": "seekTo",
+      "args": [seconds, true],
+      "id": ID
+    }), "*");
+  }
+
+  static loadAndPlayVideo(videoID) {
+    const iframe = document.getElementById(ID);
+
+    iframe.contentWindow.postMessage(JSON.stringify({
+      "event": "command",
+      "func": "loadVideoById",
+      "args": [videoID],
+      "id": ID
+    }), "*");
+  }
+
   render() {
     const videoID = this.props.videoID;
 
