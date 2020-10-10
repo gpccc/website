ed81689b0aceeae0089efb3535143d1181a7fbf3
Phu Vuong <pvuong@vuonglab.com>
2020-10-09 19:04:05 -0700

    Add type to first set of seek points

--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom';
 
 import './styles/site.css';
 
+import SeekPointType from './constants/seek-point-type';
+
 import SermonCard from './components/sermon-card';
 
 function App() {
     const sermons = [
         { youtubeVideoID: "EWQ__H_85bE", topic: "Ezra 4: Rebuilding Opposed", pastor: "Pastor Larry Lee", date: "2020-10-04", duration: "01:25:45", seekPoints: [
-            {time: "00:00:00", label: "Beginning"},
-            {time: "00:10:57", label: "Opening prayer"},
-            {time: "00:12:35", label: "O Come to the Altar"},
-            {time: "00:18:02", label: "Psalm 18:2-6, 16-17, 46, 49"},
-            {time: "00:19:20", label: "Rock of Ages (You will Stand)"},
-            {time: "00:23:46", label: "Gracefully Broken"},
-            {time: "00:28:54", label: "The Wonderful Cross"},
-            {time: "00:34:17", label: "Congregational prayer"},
-            {time: "00:36:01", label: "Communion"},
-            {time: "00:44:23", label: "Message"},
-            {time: "01:23:01", label: "Announcements"},
-            {time: "01:24:45", label: "Benediction"},
+            {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
+            {time: "00:10:57", label: "Opening prayer", type: SeekPointType.PRAYER},
+            {time: "00:12:35", label: "O Come to the Altar", type: SeekPointType.PRAISE_SONG},
+            {time: "00:18:02", label: "Psalm 18:2-6, 16-17, 46, 49", type: SeekPointType.BIBLE_VERSE},
+            {time: "00:19:20", label: "Rock of Ages (You will Stand)", type: SeekPointType.PRAISE_SONG},
+            {time: "00:23:46", label: "Gracefully Broken", type: SeekPointType.PRAISE_SONG},
+            {time: "00:28:54", label: "The Wonderful Cross", type: SeekPointType.PRAISE_SONG},
+            {time: "00:34:17", label: "Congregational prayer", type: SeekPointType.PRAYER},
+            {time: "00:36:01", label: "Communion", type: SeekPointType.COMMUNION},
+            {time: "00:44:23", label: "Message", type: SeekPointType.MESSAGE},
+            {time: "01:23:01", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
+            {time: "01:24:45", label: "Benediction", type: SeekPointType.BENEDICTION},
         ]},
         { youtubeVideoID: "uqmaE9JKGtc", topic: "Ezra 3: First Comes Sacrifice", pastor: "Pastor Larry Lee", date: "2020-09-27", duration: "01:28:35", seekPoints: []},
         { youtubeVideoID: "GA94lEQkKo0", topic: "Exodus 4:10-17: Talent", pastor: "Pastor Steven Chin", date: "2020-09-20", duration: "01:23:41", seekPoints: [] },
