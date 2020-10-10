4bbe623affe4b22116e3bda3e9101abfbfadcb66
Phu Vuong <pvuong@vuonglab.com>
2020-10-09 19:02:17 -0700

    Create seek point type enums

---
+++ src/constants/seek-point-type.js
@@ -0,0 +1,12 @@
+const SeekPointType = {
+    BEGINNING: 'Beginning',
+    BIBLE_VERSE: 'Bible verse',
+    PRAISE_SONG: 'Praise song',
+    PRAYER: 'Prayer',
+    COMMUNION: 'Communion',
+    MESSAGE: 'Message',
+    ANNOUNCEMENTS: 'Announcements',
+    BENEDICTION: 'Benediction',
+};
+
+export default SeekPointType;
