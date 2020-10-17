ce4733c7636b67019df86ee5ee26ac2172cf7b89
Phu Vuong <pvuong@vuonglab.com>
2020-10-16 21:56:48 -0700

    Remove asterisk from joint service message names

--- src/constants/mandarin-services.js
+++ src/constants/mandarin-services.js
@@ -17,7 +17,7 @@ const mandarinServices = [
         {time: "01:36:34", label: "Youth Praise Night", type: SeekPointType.VIDEO},
         {time: "01:40:06", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "Li5Pd6IXhE4", topic: "Message topic TBD*", pastor: "Pastor Peter Liu", date: "2020-10-04T18:10:00Z", duration: "01:35:33", seekPoints: [
+    { youtubeVideoID: "Li5Pd6IXhE4", topic: "Message topic TBD", pastor: "Pastor Peter Liu", date: "2020-10-04T18:10:00Z", duration: "01:35:33", seekPoints: [
         {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
         {time: "00:09:55", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:11:22", label: "Praise song 1", type: SeekPointType.PRAISE_SONG},
@@ -48,7 +48,7 @@ const mandarinServices = [
         {time: "01:26:47", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:30:06", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "R0ikzK-juhI", topic: "Message topic TBD*", pastor: "Pastor Peter Liu", date: "2020-09-20T18:10:00Z", duration: "01:40:39", seekPoints: [
+    { youtubeVideoID: "R0ikzK-juhI", topic: "Message topic TBD", pastor: "Pastor Peter Liu", date: "2020-09-20T18:10:00Z", duration: "01:40:39", seekPoints: [
         {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
         {time: "00:10:04", label: "Bible verse", type: SeekPointType.BIBLE_VERSE},
         {time: "00:11:33", label: "Opening prayer", type: SeekPointType.PRAYER},
