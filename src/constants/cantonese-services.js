6eef3b6ba10a7be3229015cbe50e3134911f126e
Phu Vuong <pvuong@vuonglab.com>
2020-10-16 20:01:28 -0700

    Remove joint Cantonese services

--- src/constants/cantonese-services.js
+++ src/constants/cantonese-services.js
@@ -1,5 +1,7 @@
 import SeekPointType from './seek-point-type';
 
+import { JOINT_SERVICE } from './sentinels'
+
 const cantoneseServices = [
     { youtubeVideoID: "Rl4hT1wifFQ", topic: "Message topic TBD", pastor: "Pastor Semson Nip", date: "2020-10-11T18:10:00Z", duration: "01:26:05", seekPoints: [
         {time: "00:00:20", label: "Beginning", type: SeekPointType.BEGINNING},
@@ -13,20 +15,7 @@ const cantoneseServices = [
         {time: "01:20:15", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:25:00", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "Li5Pd6IXhE4", topic: "Message topic TBD*", pastor: "Pastor Peter Liu", date: "2020-10-04T18:10:00Z", duration: "01:35:33", seekPoints: [
-        {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
-        {time: "00:09:55", label: "Opening prayer", type: SeekPointType.PRAYER},
-        {time: "00:11:22", label: "Praise song 1", type: SeekPointType.PRAISE_SONG},
-        {time: "00:16:54", label: "Congregational prayer?", type: SeekPointType.PRAYER},
-        {time: "00:17:57", label: "Praise song 2", type: SeekPointType.PRAISE_SONG},
-        {time: "00:21:48", label: "Praise song 3", type: SeekPointType.PRAISE_SONG},
-        {time: "00:27:06", label: "Bible verse?", type: SeekPointType.BIBLE_VERSE},
-        {time: "00:29:49", label: "Message", type: SeekPointType.MESSAGE},
-        {time: "01:20:00", label: "Communion", type: SeekPointType.COMMUNION},
-        {time: "01:26:02", label: "Communion song", type: SeekPointType.PRAISE_SONG},
-        {time: "01:30:06", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
-        {time: "01:34:02", label: "Benediction", type: SeekPointType.BENEDICTION},
-    ]},
+    { youtubeVideoID: "Li5Pd6IXhE4", topic: JOINT_SERVICE, pastor: "", date: "", duration: "", seekPoints: [] },
     { youtubeVideoID: "AlrtccfApe8", topic: "Message topic TBD", pastor: "Pastor Semson Nip", date: "2020-09-27T18:10:00Z", duration: "01:22:19", seekPoints: [
         {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
         {time: "00:08:00", label: "Opening prayer?", type: SeekPointType.PRAYER},
@@ -39,21 +28,7 @@ const cantoneseServices = [
         {time: "01:18:45", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:21:17", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "R0ikzK-juhI", topic: "Message topic TBD*", pastor: "Pastor Peter Liu", date: "2020-09-20T18:10:00Z", duration: "01:40:39", seekPoints: [
-        {time: "00:00:00", label: "Beginning", type: SeekPointType.BEGINNING},
-        {time: "00:10:04", label: "Bible verse", type: SeekPointType.BIBLE_VERSE},
-        {time: "00:11:33", label: "Opening prayer", type: SeekPointType.PRAYER},
-        {time: "00:13:10", label: "Jesus, My Everything", type: SeekPointType.PRAISE_SONG},
-        {time: "00:17:50", label: "Praise song 2", type: SeekPointType.PRAISE_SONG},
-        {time: "00:22:02", label: "Praise song 3", type: SeekPointType.PRAISE_SONG},
-        {time: "00:27:50", label: "Members singing", type: SeekPointType.VIDEO},
-        {time: "00:32:18", label: "Congregational prayer?", type: SeekPointType.PRAYER},
-        {time: "00:34:30", label: "Message", type: SeekPointType.MESSAGE},
-        {time: "01:24:06", label: "Missions at GPCCC?", type: SeekPointType.ANNOUNCEMENTS},
-        {time: "01:34:18", label: "Closing prayer", type: SeekPointType.PRAYER},
-        {time: "01:36:51", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
-        {time: "01:39:24", label: "Benediction", type: SeekPointType.BENEDICTION},
-    ]},
+    { youtubeVideoID: "R0ikzK-juhI", topic: JOINT_SERVICE, pastor: "", date: "", duration: "", seekPoints: [] },
 ];
 
 export default cantoneseServices;
---
+++ src/constants/sentinels.js
@@ -0,0 +1,1 @@
+export const JOINT_SERVICE = '(joint service)';
