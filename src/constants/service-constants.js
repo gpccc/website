3f4d92ecfa2b06ebf47ef19d51f1f79891034585
Phu Vuong <pvuong@vuonglab.com>
2020-10-18 18:29:50 -0700

    Show 5 recent services to get translations

--- src/constants/service-constants.js
+++ src/constants/service-constants.js
@@ -2,4 +2,4 @@ export const JOINT_SERVICE = '(joint service)';
 
 export const SERVICE_DURATION_IN_SECONDS = (60+20)*60;
 
-export const NUM_RECENT_SERVICES_TO_SHOW = 4;
+export const NUM_RECENT_SERVICES_TO_SHOW = 5;
