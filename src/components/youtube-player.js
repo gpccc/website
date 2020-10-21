049829d3a7a01bd2405f83c228dcb0be5c1d8019
Phu Vuong <pvuong@vuonglab.com>
2020-10-20 22:35:31 -0700

    Convert YouTubePlayer to functional component

--- src/components/youtube-player.js
+++ src/components/youtube-player.js
@@ -3,23 +3,18 @@ import PropTypes from 'prop-types';
 
 import CardMedia from '@material-ui/core/CardMedia';
 
-export default class YouTubePlayer extends React.Component {
-  render() {
-    const playerID = this.props.playerID;
-    const videoID = this.props.videoID;
-
-    return (
-      <CardMedia
-        component="iframe"
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + videoID + "?enablejsapi=1"}
-        frameBorder="0"
-        id={playerID}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-      />
-    );
-  }
+export default function YouTubePlayer({playerID, videoID}) {
+  return (
+    <CardMedia
+      component="iframe"
+      width="560"
+      height="315"
+      src={"https://www.youtube.com/embed/" + videoID + "?enablejsapi=1"}
+      frameBorder="0"
+      id={playerID}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+    />
+  );
 }
 
 YouTubePlayer.propTypes = {
