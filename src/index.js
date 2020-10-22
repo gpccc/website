d42e4bed4d6775b088bb18268b10d5095ff10bac
Phu Vuong <pvuong@vuonglab.com>
2020-10-21 21:03:31 -0700

    Delete notes of YouTube sermon IDs

--- src/index.js
+++ src/index.js
@@ -13,21 +13,8 @@ function App() {
     return (
         <React.StrictMode>
         <div>
-
           <ServiceCard showSnackbar={showSnackbar} />
           {snackbarData && <AppSnackbar msg={snackbarData.msg} key={snackbarData.date} />}
-
-        {/*
-        Li5Pd6IXhE4 10/4/2020
-        AYadEf1YjVk
-        R0ikzK-juhI
-        uOjAL6YPG3U
-        
-        AlrtccfApe8 9/27/2020
-        9/20/2020 missing - joint service with Mandarin
-        aEijmDl2Qjg
-        9/6/2020 missing - joint service with Mandarin
-        */}
         </div>
         </React.StrictMode>
     );
