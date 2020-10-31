a0e705e2b01920c555ca241ac43708d6ee6ee6a4
Phu Vuong <pvuong@vuonglab.com>
2020-10-31 15:01:18 -0700

    Move Bible citations to end of sermon titles

--- src/constants/english-services.js
+++ src/constants/english-services.js
@@ -2,7 +2,7 @@ import SeekPointType from './seek-point-type';
 
 // MjQnqIH8qmM 1280x720
 const englishServices = [
-    { youtubeVideoID: "dkLZnTDGVWw", message: "Ezra 7: The Sovereignty of God", pastor: "Pastor Jerry Lin", date: "2020-10-25T16:30:00Z", duration: "01:19:16", seekPoints: [
+    { youtubeVideoID: "dkLZnTDGVWw", message: "The Sovereignty of God (Ezra 7)", pastor: "Pastor Jerry Lin", date: "2020-10-25T16:30:00Z", duration: "01:19:16", seekPoints: [
         {time: "00:00:58", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:03:48", label: "Our God", type: SeekPointType.PRAISE_SONG},
         {time: "00:08:21", label: "John 15:9-13", type: SeekPointType.BIBLE_VERSE},
@@ -16,7 +16,7 @@ const englishServices = [
         {time: "01:15:34", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:18:12", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "MjQnqIH8qmM", message: "Ezra 6: Temple Filled with Joy", pastor: "Pastor Larry Lee", date: "2020-10-18T16:30:00Z", duration: "01:26:10", seekPoints: [
+    { youtubeVideoID: "MjQnqIH8qmM", message: "Temple Filled with Joy (Ezra 6)", pastor: "Pastor Larry Lee", date: "2020-10-18T16:30:00Z", duration: "01:26:10", seekPoints: [
         {time: "00:11:22", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:11:58", label: "Psalm 103:1-5, 8, 14-17, 22", type: SeekPointType.BIBLE_VERSE},
         {time: "00:13:17", label: "10,000 Reasons", type: SeekPointType.PRAISE_SONG},
@@ -31,7 +31,7 @@ const englishServices = [
         {time: "01:20:29", label: "Benediction", type: SeekPointType.BENEDICTION},
         {time: "01:21:38", label: "Great Things", type: SeekPointType.PRAISE_SONG},
     ]},
-    { youtubeVideoID: "gvSIj2PaVGM", message: "Ezra 5: Rebuilding Resumes", pastor: "Pastor Jerry Lin", date: "2020-10-11T16:30:00Z", duration: "01:22:55", seekPoints: [
+    { youtubeVideoID: "gvSIj2PaVGM", message: "Rebuilding Resumes (Ezra 5)", pastor: "Pastor Jerry Lin", date: "2020-10-11T16:30:00Z", duration: "01:22:55", seekPoints: [
         {time: "00:10:52", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:12:20", label: "Job 1:20-21", type: SeekPointType.BIBLE_VERSE}, 
         {time: "00:13:37", label: "Blessed Be Your Name", type: SeekPointType.PRAISE_SONG},
@@ -48,7 +48,7 @@ const englishServices = [
         {time: "01:18:55", label: "Benediction", type: SeekPointType.BENEDICTION},
         {time: "01:19:30", label: "Happy birthday, Pastor Larry", type: SeekPointType.BIRTHDAY},
     ]},
-    { youtubeVideoID: "EWQ__H_85bE", message: "Ezra 4: Rebuilding Opposed", pastor: "Pastor Larry Lee", date: "2020-10-04T16:30:00Z", duration: "01:25:45", seekPoints: [
+    { youtubeVideoID: "EWQ__H_85bE", message: "Rebuilding Opposed (Ezra 4)", pastor: "Pastor Larry Lee", date: "2020-10-04T16:30:00Z", duration: "01:25:45", seekPoints: [
         {time: "00:10:57", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:12:35", label: "O Come to the Altar", type: SeekPointType.PRAISE_SONG},
         {time: "00:18:02", label: "Psalm 18:2-6, 16-17, 46, 49", type: SeekPointType.BIBLE_VERSE},
@@ -61,7 +61,7 @@ const englishServices = [
         {time: "01:23:01", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:24:45", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "uqmaE9JKGtc", message: "Ezra 3: First Comes Sacrifice", pastor: "Pastor Larry Lee", date: "2020-09-27T16:30:00Z", duration: "01:28:35", seekPoints: [
+    { youtubeVideoID: "uqmaE9JKGtc", message: "First Comes Sacrifice (Ezra 3)", pastor: "Pastor Larry Lee", date: "2020-09-27T16:30:00Z", duration: "01:28:35", seekPoints: [
         {time: "00:10:36", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:11:38", label: "O Praise the Name", type: SeekPointType.PRAISE_SONG},
         {time: "00:16:10", label: "Center", type: SeekPointType.PRAISE_SONG},
@@ -75,7 +75,7 @@ const englishServices = [
         {time: "01:22:17", label: "Announcements", type: SeekPointType.ANNOUNCEMENTS},
         {time: "01:22:49", label: "Benediction", type: SeekPointType.BENEDICTION},
     ]},
-    { youtubeVideoID: "GA94lEQkKo0", message: "Exodus 4:10-17: Talent", pastor: "Pastor Steven Chin", date: "2020-09-20T16:30:00Z", duration: "01:23:41", seekPoints: [
+    { youtubeVideoID: "GA94lEQkKo0", message: "Talent (Exodus 4:10-17)", pastor: "Pastor Steven Chin", date: "2020-09-20T16:30:00Z", duration: "01:23:41", seekPoints: [
         {time: "00:09:57", label: "Opening prayer", type: SeekPointType.PRAYER},
         {time: "00:11:04", label: "Who You Say I Am", type: SeekPointType.PRAISE_SONG},
         {time: "00:15:34", label: "Luke 23:32-43", type: SeekPointType.BIBLE_VERSE},
