33d5a503cb5a2a92e3e5fbb071f384649166dbe1
Phu Vuong <pvuong@vuonglab.com>
2020-11-06 15:37:41 -0700

    Add 11/8/2020 Mandarin service placeholder

--- src/constants/mandarin-services.js
+++ src/constants/mandarin-services.js
@@ -1,6 +1,8 @@
 import SeekPointType from './seek-point-type';
 
 const mandarinServices = [
+    { youtubeVideoID: "hAG0ird_VQk", message: "(to be determined)", pastor: "Pastor Feng Zhongqiang", date: "2020-11-08T18:10:00Z", duration: "00:00:00", seekPoints: [
+    ]},
     { youtubeVideoID: "m9YoDp3xFSQ", message: "犹太人的王 (约19:14-19)", pastor: "Rev. Semson Nip", date: "2020-11-01T18:10:00Z", duration: "01:17:30", seekPoints: [
         // {time: "00:00:00", label: "让赞美飞扬", type: SeekPointType.PRAISE_SONG},
         {time: "00:00:00", label: "神羔羊配得", type: SeekPointType.PRAISE_SONG},
