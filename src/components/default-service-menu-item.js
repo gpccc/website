bb5af3a5f10a04c535711edf39d5d6f1801fb7f0
Phu Vuong <pvuong@vuonglab.com>
2020-11-15 12:45:23 -0700

    Refactor default service menu item into component

---
+++ src/components/default-service-menu-item.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+import MenuItem from '@material-ui/core/MenuItem';
+
+import i18n from '../i18n';
+
+import Cookies from 'js-cookie';
+
+import { SERVICE_COOKIE_KEY, COOKIE_EXPIRATION_DAYS } from '../constants/service-constants';
+
+const CANTONESE_SERVICE = 'Cantonese';
+const ENGLISH_SERVICE = 'English';
+const MANDARIN_SERVICE = 'Mandarin';
+
+const services = [
+    CANTONESE_SERVICE,
+    ENGLISH_SERVICE,
+    MANDARIN_SERVICE,
+];
+
+const guessServiceFromLanguage = (langCode) => {
+    if (langCode === 'zf') {
+        return MANDARIN_SERVICE;
+    }
+
+    if (langCode === 'zh') {
+        return CANTONESE_SERVICE;
+    }
+
+    return ENGLISH_SERVICE;
+};
+
+const getDefaultServiceCode = () => Cookies.get(SERVICE_COOKIE_KEY) || guessServiceFromLanguage(i18n.language);
+
+export default function DefaultServiceMenuItem() {
+    const [defaultService, setDefaultService] = React.useState(getDefaultServiceCode());
+
+    const handleServiceSelect = (event) => {
+        const serviceCode = event.target.value;
+        setDefaultService(serviceCode);
+
+        Cookies.set(SERVICE_COOKIE_KEY, serviceCode, { expires: COOKIE_EXPIRATION_DAYS });
+    };
+
+    return (
+        <MenuItem disableRipple>
+            <FormControl component="fieldset">
+            <FormLabel component="legend">Worship service I attend:</FormLabel>
+            <RadioGroup row aria-label="default service" name="defaultService" value={defaultService} onChange={handleServiceSelect}>
+                {services.map((service) => (
+                    <FormControlLabel key={service} value={service} control={<Radio />} label={service} />
+                ))}
+            </RadioGroup>
+            </FormControl>
+        </MenuItem>
+    );
+}
--- src/components/default-service-menu.js
+++ src/components/default-service-menu.js
@@ -1,50 +1,13 @@
 import React from 'react';
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 
-import i18n from '../i18n';
-
-import Cookies from 'js-cookie';
-
-import { SERVICE_COOKIE_KEY, COOKIE_EXPIRATION_DAYS } from '../constants/service-constants';
-
-const CANTONESE_SERVICE = 'Cantonese';
-const ENGLISH_SERVICE = 'English';
-const MANDARIN_SERVICE = 'Mandarin';
-
-const services = [
-    CANTONESE_SERVICE,
-    ENGLISH_SERVICE,
-    MANDARIN_SERVICE,
-];
-
-const guessServiceFromLanguage = (langCode) => {
-    if (langCode === 'zf') {
-        return MANDARIN_SERVICE;
-    }
-
-    if (langCode === 'zh') {
-        return CANTONESE_SERVICE;
-    }
-
-    return ENGLISH_SERVICE;
-}
-
-const getDefaultServiceCode = () => Cookies.get(SERVICE_COOKIE_KEY) || guessServiceFromLanguage(i18n.language);
+import DefaultServiceMenuItem from './default-service-menu-item';
 
 export default function DefaultServiceMenu() {
-    const [defaultService, setDefaultService] = React.useState(getDefaultServiceCode());
-
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
 
@@ -52,13 +15,6 @@ export default function DefaultServiceMenu() {
         setAnchorEl(event.currentTarget);
     };
     
-    const handleServiceSelect = (event) => {
-        const serviceCode = event.target.value;
-        setDefaultService(serviceCode);
-
-        Cookies.set(SERVICE_COOKIE_KEY, serviceCode, { expires: COOKIE_EXPIRATION_DAYS });
-    };
-
     const handleClose = () => {
         setAnchorEl(null);
     };
@@ -90,16 +46,7 @@ export default function DefaultServiceMenu() {
             open={open}
             onClose={handleClose}>
 
-            <MenuItem disableRipple>
-            <FormControl component="fieldset">
-            <FormLabel component="legend">Worship service I attend:</FormLabel>
-            <RadioGroup row aria-label="default service" name="defaultService" value={defaultService} onChange={handleServiceSelect}>
-                {services.map((service) => (
-                    <FormControlLabel key={service} value={service} control={<Radio />} label={service} />
-                ))}
-            </RadioGroup>
-            </FormControl>
-            </MenuItem>
+            <DefaultServiceMenuItem />
         </Menu>
         </div>
     );
