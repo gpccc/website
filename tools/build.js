2a07a5e0c1047502bd8b01c3eab0454d0890113a
Phu Vuong <pvuong@vuonglab.com>
2020-11-20 12:26:37 -0700

    Update webpack warning check and dump

--- tools/build.js
+++ tools/build.js
@@ -21,9 +21,8 @@ webpack(config).run((error, stats) => {
     return jsonStats.errors.map(error => console.log(chalkError(error)));
   }
 
-  if (stats.hasWarnings && jsonStats.warnings.length > 0) {
-    console.log(chalkWarning('Webpack generated the following warnings: '));
-    jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
+  if (stats.hasWarnings && jsonStats.warningsCount > 0) {
+    console.log(chalkWarning('Webpack generated the following warnings:'));
   }
 
   console.log(`Webpack stats: ${stats}`);
