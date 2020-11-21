a1eda1b6a0b729c1803b964a51740015e951f9c4
Phu Vuong <pvuong@vuonglab.com>
2020-11-20 19:15:50 -0700

    Enable redux store and make it available to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import configureStore from  './store/configureStore';
+
 import './i18n';
 
 import './styles/site.css';
@@ -9,16 +12,21 @@ import ElevateAppBar from './components/elevate-app-bar.js';
 import AppSnackbar from './components/app-snack-bar';
 import ServiceCard from './components/service-card';
 
+const initialState = {};
+const store = configureStore(initialState);
+
 function App() {
     const [snackbarData, setSnackbarData] = React.useState("");
     const showSnackbar = msg => setSnackbarData({ msg, date: new Date() });
 
     return (
         <React.StrictMode>
+        <Provider store={store}>
         <ElevateAppBar>
             <ServiceCard showSnackbar={showSnackbar} />
             {snackbarData && <AppSnackbar msg={snackbarData.msg} key={snackbarData.date} />}
         </ElevateAppBar>
+        </Provider>
         </React.StrictMode>
     );
 }
