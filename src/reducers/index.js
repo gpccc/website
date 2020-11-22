5b8b4774f8b74d83773479141b181aa7721c4900
Phu Vuong <pvuong@vuonglab.com>
2020-11-21 19:54:41 -0700

    Move list of Cantonese worship services to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import mandarinVideosReducer from './mandarin-videos-reducer';
+import cantoneseVideosReducer from './cantonese-videos-reducer';
 import englishVideosReducer from './english-videos-reducer';
 import settingsReducer from './settings-reducer';
 
 const allReducers = () => combineReducers({
     mandarinVideos: mandarinVideosReducer,
+    cantoneseVideos: cantoneseVideosReducer,
     englishVideos: englishVideosReducer,
     settings: settingsReducer,
 });
