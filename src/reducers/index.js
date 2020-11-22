d35094694352491629625f145b04bf85b017eae4
Phu Vuong <pvuong@vuonglab.com>
2020-11-21 19:42:39 -0700

    Move list of Mandarin worship services to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
+import mandarinVideosReducer from './mandarin-videos-reducer';
 import englishVideosReducer from './english-videos-reducer';
 import settingsReducer from './settings-reducer';
 
 const allReducers = () => combineReducers({
+    mandarinVideos: mandarinVideosReducer,
     englishVideos: englishVideosReducer,
     settings: settingsReducer,
 });
