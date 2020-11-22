28645829f09542df031e855130d0ea0fe511f0f3
Phu Vuong <pvuong@vuonglab.com>
2020-11-21 19:05:09 -0700

    Move list of English worship services to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
+import englishVideosReducer from './english-videos-reducer';
 import settingsReducer from './settings-reducer';
 
 const allReducers = () => combineReducers({
+    englishVideos: englishVideosReducer,
     settings: settingsReducer,
 });
 
