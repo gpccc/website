9d832d048259ade2c58b960f5398c64089cb6b90
Phu Vuong <pvuong@vuonglab.com>
2020-11-24 22:27:34 -0700

    Render the card with the church map

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import ElevateAppBar from './components/elevate-app-bar.js';
 import AppSnackbar from './components/app-snack-bar';
 import ServiceCardContainer from './components/service-card-container';
 
-import ChurchMap from './components/church-map';
+import ChurchMapCard from './components/church-map-card';
 
 const initialState = {};
 const store = configureStore(initialState);
@@ -26,7 +26,7 @@ function App() {
         <Provider store={store}>
         <ElevateAppBar>
             <ServiceCardContainer showSnackbar={showSnackbar} />
-            <ChurchMap />
+            <ChurchMapCard />
             {snackbarData && <AppSnackbar msg={snackbarData.msg} key={snackbarData.date} />}
         </ElevateAppBar>
         </Provider>
