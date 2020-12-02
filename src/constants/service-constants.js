33f075990e8477a8cc3d781931c169e55e4a23fa
Phu Vuong <pvuong@vuonglab.com>
2020-12-01 18:55:22 -0700

    Show the most recent four sermons on services menu

--- src/constants/service-constants.js
+++ src/constants/service-constants.js
@@ -2,7 +2,7 @@ export const JOINT_SERVICE = '(joint service)';
 
 export const SERVICE_DURATION_IN_SECONDS = (60+20)*60;
 
-export const NUM_RECENT_SERVICES_TO_SHOW = 10;
+export const NUM_RECENT_SERVICES_TO_SHOW = 4;
 
 export const SERVICE_CARD_MAX_WIDTH = 345;
 
