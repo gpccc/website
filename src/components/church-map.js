496c03d2ae435e888975826ffcebd9ee0ac251d4
Phu Vuong <pvuong@vuonglab.com>
2020-12-05 17:26:30 -0700

    Show square map on mobile, rectangle on others

--- src/components/church-map.js
+++ src/components/church-map.js
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ChurchMap({mapWidth}) {
+    if (typeof mapWidth === 'undefined')  {
+        mapWidth = 320;
+    }
+    const mapHeight = (window.innerWidth >= 768) ? mapWidth / 1.618 : mapWidth;
+
+    // TODO: The Directions option is shown only on tablet and desktop (width 768 and up). Grrr...
     return (
     <iframe
         width={mapWidth}
-        height="450"
+        height={mapHeight}
         frameBorder="0" style={{border: 0}}
         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBx4Fh1nnGqr5BI2y_pInKnDmuMBRr0L5g&q=place_id:ChIJFflyL1YHK4cR57bArk8BXLM&zoom=16&language=en-US"
         allowFullScreen>
@@ -15,7 +21,7 @@ function ChurchMap({mapWidth}) {
 }
 
 ChurchMap.propTypes = {
-    mapWidth: PropTypes.number.isRequired,
+    mapWidth: PropTypes.number,
 };
 
 export default ChurchMap;
