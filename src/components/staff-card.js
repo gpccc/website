23c29ce76a80bf217dbff468b12c90574760b010
Phu Vuong <pvuong@vuonglab.com>
2020-12-15 23:25:19 -0700

    Center staff name

--- src/components/staff-card.js
+++ src/components/staff-card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -35,8 +36,10 @@ export default function StaffCard() {
         />
         <div className={classes.details}>
             <CardContent className={classes.content}>
-                <Typography component="h5" variant="h5">
-                    Jerry Lin
+                <Typography variant="h5">
+                    <Box textAlign="center">
+                        Jerry Lin
+                    </Box>
                 </Typography>
                 <Typography variant="subtitle1" color="textSecondary">
                     Favorite verse is Genesis 2:18, as this verse played a key role in God&apos;s call on his life.
